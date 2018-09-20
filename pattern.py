87a9d3d22bbdbbac77e3d5a56c3ba59d71b989c5
andrewli315 <nyps70414@gmail.com>
2018-09-20 11:49:33 +0800

    complete

--- pattern.py
+++ pattern.py
@@ -1,5 +1,5 @@
 import sys
-MAX_PATTERN_LENGTH = 26*26*10
+MAX_PATTERN_LENGTH = 26*26*10*3
 def pattern_generator(length):
 	"""
 	Generate a pattern in order to calculate the offset of stack.
@@ -14,10 +14,16 @@ def pattern_generator(length):
 	_third = '0'
 	pattern = ''
 	for i in range(26):
+		_first = 'A'
+		_second = 'a'
+		_third = '0'
 		_first = chr(ord(_first) + i )
 		for j in range(26):
+			_second = 'a'
+			_third = '0'
 			_second = chr(ord(_second) + j)
 			for k in range(10):
+				_third = '0'
 				_third = chr(ord(_third) + k)
 				if( len(pattern) < int(length)):
 					pattern += _first+_second+_third
@@ -34,13 +40,58 @@ def search_pattern(input_pattern):
 	Searching the pattern and calculate the offset 
 	from the pattern this generator generated.
 	"""
+	#input = str(int(input_pattern,16))
+	tmp = ''
+	i = 0
+	s = ''
+	input = ''
+	input = str(input_pattern)
+	input = input.replace('0x','')
+	while (i < len(input)):
+		tmp = input[i:i+2]
+		t = int(tmp,16)
+		c = chr(t)
+		s += c;
+		i += 2
+	# reverse the string because the pattern is little endian
+	# we need to reverse the string finding the
+	s = s[::-1]
+	print('hex pattern decoded as : %s' % s)
+	_first = 'A'
+	_second = 'a'
+	_third = '0'
+	findString = ''
+	for i in range(26):
+		_first = 'A'
+		_second = 'a'
+		_third = '0'
+		_first = chr(ord(_first) + i )
+		for j in range(26):
+			_second = 'a'
+			_third = '0'
+			_second = chr(ord(_second) + j)
+			for k in range(10):
+				_third = '0'
+				_third = chr(ord(_third) + k)
+				findString += _first + _second + _third
+				find_index = findString.find(s)
+				if( find_index > -1):
+					print('offset = %d' % find_index)
+					return find_index
+					
+
+
+
+
+
+
 def help():
 	print ('Usage: %s [-g]LENGTH [-s]PATTERN ' % sys.argv[0])
 	print ()
 	print ('-g \t (or --generate) \tGenerate a pattern, such as Aa0Aa1...')
 	print ('-s \t (or --search)   \tSearch offset from input pattern ')
 
-if _name_ == '_main_':
+if __name__ == '__main__':
 
 	if( len(sys.argv) < 3):
 		help()
